893f3a0f8f8fcc9e2b942f51917614ff8e47d460
Patrick <patrickdsilva99@gmail.com>
2019-02-03 16:50:13 -0200

    postgres

--- django_test/settings.py
+++ django_test/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import keys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -75,7 +76,10 @@ WSGI_APPLICATION = 'django_test.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST' : 'localhost',
+        'USER' : 'pgadmin',
+        'PASSWORD' : keys.postgres,
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
